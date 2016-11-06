5cade4668fd3f6cd575d1ae46d40a72dbcf85d4c
Rolian Ardi <rolianardi@gmail.com>
2016-11-06 09:18:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require('function.php');
+require('connection.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/index.html">
+<title>BPS Social Media</title>
+<script language="javascript">
+    window.location.href = "pages/index.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/index.php">/pages/index.php</a>
+</body>
+</html>
